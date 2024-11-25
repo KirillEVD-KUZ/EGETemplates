6ec5491b878e054bd4a53273f84b8b04f489d172
kkcuz <kkcuznetzpv@yandex.ru>
2024-11-25 18:24:20 +0300

    Решение задач

---
+++ Тема 13/18588.py
@@ -0,0 +1,5 @@
+ip = '98.162.71.151'
+print(' '.join([bin(int(o))[2:].zfill(8) for o in ip.split('.')]))
+print()
+ip = '98.162.71.155'
+print(' '.join([bin(int(o))[2:].zfill(8) for o in ip.split('.')]))
